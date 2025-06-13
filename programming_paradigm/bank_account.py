695cf84a7361836b5283ed5a9873965d354a9731
Gucha_duncan <gucha_duncan@outlook.com>
2025-06-13 13:14:47 +0300

    Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,13 +16,12 @@ class BankAccount:
     def withdraw(self,amount):
         if amount <= self.__account_balance:
           self.__account_balance   -= amount
-        return True
-    
-       
-       
+          return True
+        else:
+            return False
     
     def display_balance(self):
         
-        return f"Your account balance is {self.__account_balance}"
+         print(f"Current Balance: ${self.__account_balance:.2f}")
         
     
---
+++ quiz_app/questions.py
@@ -0,0 +1,2 @@
+with open("quiz_answer.txt", 'r') as file:
+    print(file.read())
