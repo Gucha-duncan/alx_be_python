2f90e28c547f95b5154d1c920b1f9a0cd14ac22a
Gucha_duncan <gucha_duncan@outlook.com>
2025-06-13 11:33:27 +0300

    Robust Division Calculator with Command Line Arguments

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.__account_balance = account_balance
+        
+        
+    def get_account_balance(self):
+        return self.__account_balance
+    def set_account_balance(self, account_balance):
+        if account_balance > 0:
+            self.__account_balance = account_balance
+        else:
+            print("Your account balance is zero!")
+    
+    def deposit(self, amount):
+        self.__account_balance += amount
+    def withdraw(self,amount):
+        if amount <= self.__account_balance:
+          self.__account_balance   -= amount
+        return True
+    
+       
+       
+    
+    def display_balance(self):
+        
+        return f"Your account balance is {self.__account_balance}"
+        
+    
